44e89aa1ea0758ae12c464c30eb87c91840e36d2
kohei-tofu <fukouhei001@gmail.com>
2021-11-11 20:26:35 +0900

    update

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ console.log(process.env)
 // exports.HOST_URL = process.env.API_URL || "http://localhost:1112"
 // exports.API_POST = process.env.API || "keypoint/image-raw"
 
-exports.HOST_URL = process.env.API_URL || "http://localhost:1110"
+// exports.HOST_URL = process.env.API_URL || "http://localhost:1110"
 // exports.HOST_URL = process.env.API_URL || "http://mymediapipeserver"
-// exports.HOST_URL = process.env.API_URL || "https://mykeypointserver.kevin-asobi.com"
+exports.HOST_URL = process.env.API_URL || "https://mykeypointserver.kevin-asobi.com"
 
 // exports.API_POST = process.env.API || "image2skeleton"
 exports.API_POST = process.env.API || "skeleton_image"
